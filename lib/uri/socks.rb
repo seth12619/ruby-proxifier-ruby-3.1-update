904a8c5c203765001245cc1e3a41b8d4aa0a0f7c
Seth <seth12619@users.noreply.github.com>
2024-04-16 05:07:59 +0800

    Use register scheme instead of @@scheme

--- lib/uri/socks.rb
+++ lib/uri/socks.rb
@@ -5,14 +5,14 @@ module URI
     DEFAULT_PORT = 1080
     COMPONENT = [:scheme, :userinfo, :host, :port, :query].freeze
   end
-  @@schemes["SOCKS"] = SOCKS
-  @@schemes["SOCKS5"] = SOCKS
+  register_scheme "SOCKS", SOCKS
+  register_scheme "SOCKS5", SOCKS
 
   class SOCKS4 < SOCKS
   end
-  @@schemes["SOCKS4"] = SOCKS4
+  register_scheme "SOCKS4", SOCKS4
 
   class SOCKS4A < SOCKS
   end
-  @@schemes["SOCKS4A"] = SOCKS4A
+  register_scheme "SOCKS4A", SOCKS4A
 end
